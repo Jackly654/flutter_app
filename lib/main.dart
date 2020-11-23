db1484afa831e331d4d20a429226a36c11d88c6f
Jackly654 <jackly65431@gmail.com>
2020-11-23 10:35:42 +0800

    git test drawer container and delete testmain

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'modle/post.dart';
+import './demo/listview_demo.dart';
 void main() =>runApp(App());
 
 class App extends StatelessWidget{
@@ -7,66 +7,69 @@ class App extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
       theme: ThemeData(
-        primarySwatch: Colors.red
+        primarySwatch: Colors.red,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        splashColor: Colors.white70
       ),
     );
   }
 }
 
-class Hello extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-   return Center(
-     child: Text(
-         'Hello111',
-         textDirection: TextDirection.ltr,
-         style: TextStyle(
-           fontSize: 40.0,
-           fontWeight: FontWeight.bold,
-           color: Colors.black26,
-         )
-     ),
-   );
-  }
-}
 
 class Home extends StatelessWidget{
-  Widget _listItemBuilder(BuildContext context, int index){
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-           posts[index].title,
-            style: Theme.of(context).textTheme.title
-          ),
-          Text(
-              posts[index].author,
-              style: Theme.of(context).textTheme.subtitle1
-          ),
-          SizedBox(height: 16.0),
-        ],
-      )
-    );
-  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('GGGGG'),
-        elevation: 40.0,
-      ),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: _listItemBuilder,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigration',
+            onPressed: () => debugPrint('Navigration button is pressed.'),
+            ),
+         title: Text('GGGGG'),
+         actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: () => debugPrint('Search button is pressed.'),
+            ),
+         ],
+          elevation: 40.0,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ]
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black26,),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black26,),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black26,),
+          ],
+        ),
+        drawer: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('this is drawer test')
+            ],
+          )
+        )
       )
-    );
+     );
   }
 }
+
