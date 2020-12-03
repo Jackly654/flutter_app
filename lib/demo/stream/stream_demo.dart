8a2e5a5ee4ae91057559048300dca6e1d73fd41f
Jackly654 <jackly65431@gmail.com>
2020-12-03 21:49:00 +0800

    streambuilder 构建widget

--- lib/demo/stream/stream_demo.dart
+++ lib/demo/stream/stream_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class StreamDemo extends StatelessWidget {
@@ -18,11 +20,46 @@ class StreamDemoHome extends StatefulWidget {
 }
 
 class StreamDemoHomeState extends State<StreamDemoHome> {
+  StreamSubscription _streamDemoSubscription;
+  StreamController<String> _streamDemo;
+  StreamSink _sinkDemo;
+  String _data = '...';
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('StreamDemoHome'),
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Text(_data),
+            StreamBuilder(
+                stream: _streamDemo.stream,
+                initialData: '....',
+                builder: (context, snapshot) => Text('${snapshot.data}')),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton(
+                  child: Text('Add'),
+                  onPressed: _addDataToStream,
+                ),
+                FlatButton(
+                  child: Text('Pause'),
+                  onPressed: _pauseStream,
+                ),
+                FlatButton(
+                  child: Text('Resume'),
+                  onPressed: _resumeStream,
+                ),
+                FlatButton(
+                  child: Text('Cancel'),
+                  onPressed: _cancelStream,
+                ),
+              ],
+            ),
+          ]
+        ),
       ),
     );
   }
@@ -33,12 +70,39 @@ class StreamDemoHomeState extends State<StreamDemoHome> {
 
     print('Create a stream');
 
-    Stream<String> _streamDemo = Stream.fromFuture(fetchData());
+    //Stream<String> _streamDemo = Stream.fromFuture(fetchData());
+    //_streamDemo = StreamController<String>();
+    _streamDemo = StreamController.broadcast();
+    _sinkDemo = _streamDemo.sink;
 
     print('Start listening on a stream');
-    _streamDemo.listen(onData, onError: onError, onDone: onDone);
+    _streamDemoSubscription =
+      _streamDemo.stream.listen(onData, onError: onError, onDone: onDone);
+
+    _streamDemo.stream.listen(onDataTwo, onError: onError, onDone: onDone);
 
     print('Initialize complete.');
+  }
+
+  void _addDataToStream() async{
+    print('Add data to stream.');
+    String data = await fetchData();
+    //_streamDemo.add(data);
+    _sinkDemo.add(data);
+  }
+  void _pauseStream(){
+    print('Pause subscription');
+    _streamDemoSubscription.pause();
+  }
+
+  void _resumeStream(){
+    print('Resume subscription');
+    _streamDemoSubscription.resume();
+  }
+
+  void _cancelStream(){
+    print('Cancel subscription');
+    _streamDemoSubscription.cancel();
   }
 
   void onError(error){
@@ -50,18 +114,27 @@ class StreamDemoHomeState extends State<StreamDemoHome> {
   }
 
   void onData(String data){
+    setState(() {
+      _data = data;
+    });
     print('$data');
   }
+
+  void onDataTwo(String data){
+    print('onDataTwo :$data');
+  }
+
   Future<String> fetchData() async {
-    await Future.delayed(Duration(seconds: 3));
-    //return 'hello!';
-    throw 'Something wrong!?';
+    await Future.delayed(Duration(seconds: 5));
+    return 'hello!';
+    //throw 'Something wrong!?';
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
+    _streamDemo.close();
   }
 
   @override
